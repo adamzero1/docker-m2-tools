8b12df0cea51781a299dfa62ed0e46c0c87ea652
adamzero1 <adam.crossland@zero1.co.uk>
2016-09-20 21:13:27 +0100

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -3,6 +3,18 @@
 service rsyslog start
 service postfix start
 
+# Add user
+useradd -U -m -u ${DEFAULT_USER_UID} -G sudo,www-data -d /home/magento ${DEFAULT_USER}
+# User specifics
+echo "" >> /home/magento/.bashrc
+echo "#Zero1 - Additions" >> /home/magento/.bashrc
+echo "export TERM=xterm" >> /home/magento/.bashrc
+echo "[client]" >> /home/magento/.my.cnf
+echo "host=${MYSQL_HOST_ALIAS}" >> /home/magento/.my.cnf
+echo "user=${MYSQL_USER}" >> /home/magento/.my.cnf
+echo "password=${MYSQL_PASSWORD}" >> /home/magento/.my.cnf
+echo "database=${MYSQL_DATABASE}" >> /home/magento/.my.cnf
+
 tail -f -n0 /etc/hosts
 
 su --login www-data --shell=/bin/bash
